78f14d00fc8ea401cba2f81c9a4dd0ea4825e8da
Justyna Stojek <bebe6295@gmail.com>
2019-11-11 17:34:43 +0100

    Embeded letters

---
+++ PracaDyplomowa.Mobile/PracaDyplomowa.Mobile/ViewModels/WritingGameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracaDyplomowa.Mobile.ViewModels
+{
+    public class WritingGameViewModel
+    {
+        public WritingGameViewModel()
+        {
+
+        }
+
+    }
+}
