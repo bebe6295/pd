e059d7221df4d112a2b5f99b8921bcaf1bc13e37
Justyna Stojek <bebe6295@gmail.com>
2019-12-03 06:44:15 +0100

    Add Mobile Http APi client

---
+++ PracaDyplomowa.Mobile/PracaDyplomowa.Mobile/Services/MobileApiService.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using System.IO;
+
+namespace PracaDyplomowa.Mobile.Services
+{
+    public class MobileApiService
+    {
+        private readonly IRestClient _restClient;
+        string _urlBase = "";
+
+        public MobileApiService(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public string CheckWriting(byte[] file)
+        {
+            var request = new RestRequest("api/WritingRecognition/CheckWriting", Method.POST);
+            request.AddFileBytes("photo", file, "photo");
+            
+            var response = _restClient.Execute<CheckWritingResponse>(request);
+
+            return response.Data.Text;
+        }
+
+        public class CheckWritingResponse
+        {
+            public string Text { get; set; }
+        }
+    }
+}
